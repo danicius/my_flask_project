e08769d5d5b2d824d397cd238e16a18f059cb912
danicius <daniellealvarez00@gmail.com>
2023-10-15 01:21:20 -0400

    oct 15th commit

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from google.cloud import speech_v1 as speech
 from google.cloud import dialogflow
 from google.cloud.dialogflow_v2 import SessionsClient
 from google.cloud.dialogflow_v2.types import TextInput, QueryInput
+from langdetect import detect
+
 
 
 
 app = Flask(__name__)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS_DIALOGFLOW"] = "response_dialog.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS_SPEECH"] = "sa_speech.json"
 
 DIALOGFLOW_CREDENTIALS = "response_dialog.json"
 SPEECH_TO_TEXT_CREDENTIALS = "sa_speech.json"
@@ -24,6 +28,26 @@ def get_response():
     
     return jsonify({'message': bot_response})
 
+# def get_dialogflow_response(text):
+#     project_id = "multilangproject"
+#     session_id = "test_session"  # This should be unique for each user or session
+    
+#     session_client = SessionsClient()
+#     session = session_client.session_path(project_id, session_id)
+    
+#     #i want english and spanish
+#     detected_language = detect(text)
+#     if detected_language == "es":
+#         text_input = TextInput(text=text, language_code="es")
+#     else:
+#         text_input = TextInput(text=text, language_code="en")
+        
+#     query_input = QueryInput(text=text_input)
+
+#     response = session_client.detect_intent(session=session, query_input=query_input)
+
+#     return response.query_result.fulfillment_text
+
 def get_dialogflow_response(text):
     project_id = "multilangproject"
     session_id = "test_session"  # This should be unique for each user or session
@@ -31,16 +55,31 @@ def get_dialogflow_response(text):
     session_client = SessionsClient()
     session = session_client.session_path(project_id, session_id)
 
-    text_input = TextInput(text=text, language_code="en-US")
+    # Detect the language of the text
+    detected_language = detect(text)
+
+    # Default to English
+    language_code = 'en-US'
+    
+    # If detected Spanish, change the language_code
+    if detected_language == 'es':
+        language_code = 'es'
+
+    text_input = TextInput(text=text, language_code=language_code)
     query_input = QueryInput(text=text_input)
+
     response = session_client.detect_intent(session=session, query_input=query_input)
+    print(f"Detected language: {detected_language}")
+    print(f"Language code: {language_code}")
 
     return response.query_result.fulfillment_text
-
-
 @app.route('/')
+def home():
+     return render_template("home.html")
+@app.route('/i')
 def index():
     return render_template("index.html")
+
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe_audio():
@@ -62,7 +101,8 @@ def transcribe_audio():
     for result in response.results:
         transcript += result.alternatives[0].transcript
 
-    return jsonify({'transcript': transcript})
+    return jsonify({'transcript': transcript}
+)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -52,3 +52,47 @@ function startDictation() {
         }
     }
 }
+// function startDictation() {
+//     if (window.hasOwnProperty('webkitSpeechRecognition')) {
+//         const recognition = new webkitSpeechRecognition();
+//         recognition.continuous = false;
+//         recognition.interimResults = false;
+//         recognition.lang = "en-US";
+//         recognition.start();
+
+//         recognition.onresult = function (e) {
+//             // Get the transcribed text
+//             const transcribedText = e.results[0][0].transcript;
+//             fetch('/transcribe', {
+//                 method: 'POST',
+//                 body: capturedAudio
+//             })
+//             .then(response => response.json())
+//             .then(data => {
+//                 const transcribedText = data.transcript;
+//                 //document.getElementById('searchWidgetTrigger').submit();
+//                 document.getElementById('searchWidgetTrigger').value = transcribedText;
+//                 sendMessage();
+//             })
+//             .catch(error => {
+//                 console.error("Error sending audio to backend:", error);
+//             });
+//         };
+//             // Input the transcribed text into the Gen App Builder search input
+//             //document.getElementById('searchWidgetTrigger').value = transcribedText;
+
+//             // TODO: Programmatically trigger the Gen App Builder search if possible
+//             // This might depend on the widget's functionality and available methods.
+//             //document.getElementById('searchWidgetTrigger').click();
+//            // document.getElementById('searchWidgetTrigger').value = transcribedText;
+//             //document.getElementById('searchWidgetTrigger').submit();
+
+
+//         };
+
+//         recognition.onerror = function (e) {
+//             recognition.stop();
+//         }
+//     }
+
+
